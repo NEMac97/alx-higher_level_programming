b6e45f2f4d5d788e2867384125a2f9992951db2a
NEMac97 <ezekponnaemeka@gmail.com>
2023-01-21 10:03:41 -0800

    Python import modules

--- 0x02-python-import_modules/calculator_1.py
+++ 0x02-python-import_modules/calculator_1.py
@@ -1,16 +1,19 @@
 #!/usr/bin/python3
 def add(a, b):
+
     """My addition function
     Args:
         a: first integer
         b: second integer
-    Returns:
+    Returns:7
         The return value. a + b
     """
+
     return (a + b)
 
 
 def sub(a, b):
+
     """My subtraction function
     Args:
         a: first integer
@@ -18,10 +21,12 @@ def sub(a, b):
     Returns:
         The return value. a - b
     """
+
     return (a - b)
 
 
 def mul(a, b):
+
     """My multiplication function
     Args:
         a: first integer
@@ -29,10 +34,12 @@ def mul(a, b):
     Returns:
         The return value. a * b
     """
+
     return (a * b)
 
 
 def div(a, b):
+
     """My division function
     Args:
         a: first integer
@@ -40,4 +47,5 @@ def div(a, b):
     Returns:
         The return value. a / b
     """
+
     return int(a / b)
